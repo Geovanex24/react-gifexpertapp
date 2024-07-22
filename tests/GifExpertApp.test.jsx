292ba492b5132890ef20583ab1456914d093bb2f
geovanex24 <geovanex24@hotmail.com>
2024-07-21 22:35:36 -0600

    Tarea: Agregar nueva categoría y no permitir duplicadas

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -15,5 +15,26 @@ describe("Pruebas en <GifExpertApp />", () => {
     // screen.debug();
   });
 
-  // TODO: Agregar más tests.
+  test("test: se debe agregar una nueva categoría", () => {
+    render(<GifExpertApp />);
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
+
+    expect(screen.getByText(inputValue)).toBeTruthy();
+  });
+
+  test("test: no se deben agregar categorías duplicadas", () => {
+    render(<GifExpertApp />);
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+
+    fireEvent.input(input, { target: { value: "One Punch" } });
+    fireEvent.submit(form);
+
+    expect(screen.getAllByText("One Punch").length).toBe(1);
+    // screen.debug();
+  });
 });
